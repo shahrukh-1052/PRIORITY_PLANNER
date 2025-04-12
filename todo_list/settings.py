7c7449fdae55b78991b96187570aa90bf35e1515
MOHAMMED HABEEBUDDIN <tufgamerz56@gmail.com>
2025-04-12 11:09:46 +0530

    corrected settings.py and .env file code

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
